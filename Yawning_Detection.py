1c411ba11e29d88a1a1b7d59024a42bbef09b1fe
Yash Srivastava <yash.srivastava_cs18@gla.ac.in>
2020-09-14 20:15:45 +0530

    yawn detection completed

--- Yawning_Detection.py
+++ Yawning_Detection.py
@@ -1,20 +1,19 @@
 import cv2
 import dlib
-import numpy as np
 from pygame import mixer
 import time
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 cap = cv2.VideoCapture(0)
-yawn_count = 0
-count_yawn = 0
+counter = 0  # Counter to initialize yawn_timer
+count_yawn = 0  # Total number of yawns
 elapsed = 0
 timer_start = 0
 yawn_timer = 0
 while True:
 
-    ret, frame = cap.read()
+    ret, frame = cap.read()  # Reading frames from the video (Web cam)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(gray)
@@ -30,29 +29,29 @@ while True:
         if lip_down - lip_up > 10:
 
             # print("Yawn")
-            yawn_count += 1
-            if yawn_count == 1:
+            counter += 1
+            if counter == 1:
                 yawn_timer = time.time()
         else:
             # print("Close")
-            yawn_count = 0
+            counter = 0
             elapsed = time.time() - yawn_timer
             yawn_timer = 0
             if 5 < elapsed < 30:
                 count_yawn += 1
                 # print(count_yawn)
-                
+
         if count_yawn == 0:
             timer_start = time.time()
             # print(start2)
-            
+
         timer_end = time.time() - timer_start
         print(timer_end)
-        
+
         if 1800 < timer_end < 1820:
             count_yawn = 0
             # print(count_yawn)
-            
+
         if count_yawn == 5:
             mixer.init()
             mixer.music.load("alarm.wav")
