ffe3b80eb014ffdfe76a3bc853493819c4463de0
sameeksharathi <rathisameeksha9@gmail.com>
2020-11-17 20:30:39 +0530

    updated

--- Demo/Facial Landmarks.py
+++ Demo/Facial Landmarks.py
@@ -3,7 +3,7 @@ import dlib
 
 detector = dlib.get_frontal_face_detector()
 
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor("../Files/shape_predictor_68_face_landmarks.dat")
 cap = cv2.VideoCapture(0)
 
 while True:
