19dfd80a53fe8106a4edcc3cf471fa5aaa00061d
sameeksharathi <rathisameeksha9@gmail.com>
2020-11-17 20:29:46 +0530

    updated

---
+++ Demo/Facial Landmarks.py
@@ -0,0 +1,31 @@
+import cv2
+import dlib
+
+detector = dlib.get_frontal_face_detector()
+
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+
+    for face in faces:
+        landmarks = predictor(gray, face)
+        nose = landmarks.parts()[27]
+        for point in landmarks.parts():
+            cv2.circle(frame, (point.x, point.y), 2, (255, 0, 0), 3)
+
+    if ret:
+        cv2.imshow("My Screen", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
